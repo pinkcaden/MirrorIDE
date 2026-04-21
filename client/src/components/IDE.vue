848f8c05d15ecc3540ab3bb595196f6965074880
adwyer02 <aidandyr@gmail.com>
2026-04-21 14:55:12 -0500

    better IDE button rendering

--- client/src/components/IDE.vue
+++ client/src/components/IDE.vue
@@ -6,19 +6,19 @@
           <div class="container m-0 p-0">
             <div class="row m-0 p-0">
               <div :style="{display: 'inline-flex', flexDirection : 'row'}">
-                <button ref="jsBtn" v-if="js" @click="this.currentLanguage = 'js'" class="langBtn">JS</button>
-                <button ref="cssBtn" v-if="css" @click="this.currentLanguage = 'css'" class="langBtn">CSS</button>
-                <button ref="htmlBtn" v-if="html" @click="this.currentLanguage = 'html'" class="langBtn">HTML</button>
+                <button ref="jsBtn" :style="currentLanguage === 'js' ? {fontWeight: 'bold', background: '#F7ECBE'} : {fontWeight: 'normal', background: '#EDBF2D'}" v-if="js" @click="currentLanguage = 'js'" class="langBtn">JS</button>
+                <button ref="cssBtn" :style="currentLanguage === 'css' ? {fontWeight: 'bold', background: '#D5E4ED'} : {fontWeight: 'normal', background: '#2687D1'}" v-if="css" @click="currentLanguage = 'css'" class="langBtn">CSS</button>
+                <button ref="htmlBtn" :style="currentLanguage === 'html' ? {fontWeight: 'bold', background: '#E3D0B6'} : {fontWeight: 'normal', background: '#D98114'}" v-if="html" @click="currentLanguage = 'html'" class="langBtn">HTML</button>
               </div>
             </div>
 
             <div class="row ps-4">
               <TextEditor ref="cssEditor" v-if="css" v-bind:lang-choice="'css'" :parentFunc = "this.handleShare"
-                          v-show="this.currentLanguage === 'css'" ></TextEditor>
+                          v-show="currentLanguage === 'css'" ></TextEditor>
               <TextEditor ref="htmlEditor" v-if="html" v-bind:lang-choice="'html'" :parentFunc = "this.handleShare"
-                          v-show="this.currentLanguage === 'html'"></TextEditor>
+                          v-show="currentLanguage === 'html'"></TextEditor>
               <TextEditor ref="jsEditor" v-if="js" v-bind:lang-choice="'js'" :parentFunc = "this.handleShare"
-                          v-show="this.currentLanguage === 'js'"></TextEditor>
+                          v-show="currentLanguage === 'js'"></TextEditor>
             </div>
             <div/>
           </div>
@@ -53,11 +53,6 @@ import TextEditor from './ide/TextEditor.vue'
 import Terminal from './ide/Terminal.vue'
 import RunButton from './ide/RunButton.vue';
 import Runner from '../utils/execution/Runner.js'
-const BUTTON_COLORS = {
-  'js': {selected: "#F7ECBE", unselected: "#EDBF2D"},
-  'css': {selected: "#D5E4ED", unselected: "#2687D1"},
-  'html': {selected: "#E3D0B6", unselected: "#D98114"}
-}
 export default {
   components: {
     RunButton, TextEditor, Terminal
@@ -81,13 +76,13 @@ export default {
   },
   mounted() {
     if (this.js) {
-      this._languages['js'] = {button: this.$refs.jsBtn, editor: this.$refs.jsEditor, ...BUTTON_COLORS['js']};
+      this._languages['js'] = {button: this.$refs.jsBtn, editor: this.$refs.jsEditor};
     }
     if (this.html) {
-      this._languages['html'] = {button: this.$refs.htmlBtn, editor: this.$refs.htmlEditor, ...BUTTON_COLORS['html']}
+      this._languages['html'] = {button: this.$refs.htmlBtn, editor: this.$refs.htmlEditor}
     }
     if (this.css) {
-      this._languages['css'] = {button: this.$refs.cssBtn, editor: this.$refs.cssEditor, ...BUTTON_COLORS['css']}
+      this._languages['css'] = {button: this.$refs.cssBtn, editor: this.$refs.cssEditor}
     }
     for (const lang in Object.keys(this._languages)) {
       this.currentLanguage = lang
@@ -109,21 +104,6 @@ export default {
     }, 150)
   },
   watch: {
-    currentLanguage(newValue, oldValue) {
-      if (oldValue === undefined) {
-        for (const lang of Object.values(this._languages)) {
-          lang.button.style.background = lang.unselected
-          lang.button.style.fontWeight = 'normal'
-        }
-      } else {
-        const oldLang = this._languages[oldValue]
-        oldLang.button.style.background = oldLang.unselected
-        oldLang.button.style.fontWeight = 'normal'
-      }
-      const newLang = this._languages[newValue]
-      newLang.button.style.background = newLang.selected
-      newLang.button.style.fontWeight = 'bold'
-    },
     darkMode(newValue) {
       for (const lang of Object.values(this._languages)) {
         lang.editor.setDarkMode(newValue)
